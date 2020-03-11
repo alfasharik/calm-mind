1819c4811cab8e8e73c1a80c3c7415475f9b2c3e
alfalllapuk <juggy110@gmail.com>
2020-03-11 20:21:41 +0300

    Первые эксперименты с blade.

---
+++ resources/views/feed.blade.php
@@ -0,0 +1,12 @@
+@extends('homepage')
+
+@section('title', 'Feed')
+
+@section('sidebar')
+    @parent
+    <p>ahahah</p>
+@endsection
+
+@section('content')
+    <p>This is child page.</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/homepage', function () {
+   return view('homepage');
+});
+
+Route::get('/feed', function () {
+    return view('feed');
+});
